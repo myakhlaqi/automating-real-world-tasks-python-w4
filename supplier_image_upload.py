35b98ed0cec3dfcb4a438994be4126a6bf3bbbea
Mohammad Yahya Akhlaqi <91276692+myakhlaqi@users.noreply.github.com>
2022-01-31 15:16:16 +0800

    Update supplier_image_upload.py

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -16,7 +16,6 @@ def get_image_paths(mypath):
 
 url = "http://35.226.214.171/upload/"
 
-
 #with open('/usr/share/apache2/icons/icon.sheet.png', 'rb') as opened:
     #r = requests.post(url, files={'file': opened})
 
