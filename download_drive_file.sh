19b231e84470900cbea50e4e190276c63e67e27d
Mohammad Yahya Akhlaqi <91276692+myakhlaqi@users.noreply.github.com>
2022-02-05 10:49:23 +0800

    Update download_drive_file.sh

--- download_drive_file.sh
+++ download_drive_file.sh
@@ -11,6 +11,7 @@ COOKIE_FILE=$(mktemp cookiesXXXX.txt)
 
 CONFIRM=$(wget --quiet --save-cookies ${COOKIE_FILE} --keep-session-cookies "https://docs.google.com/uc?export=download&id=${FILEID}" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
 
+
 # Then download the file using the confirmation prompt
 wget --load-cookies ${COOKIE_FILE} "https://docs.google.com/uc?export=download&confirm=${CONFIRM}&id=${FILEID}" -O ${FILENAME}
 
